7668253fecbbc420f2bc7a069a8e79c153180a68
Shashwat Singh <shashwats17@gmail.com>
2024-07-10 10:45:35 -0400

    Reversed

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ with open ("names.txt") as skib:
     for line in skib:
         names.append(line.rstrip())
 
-for name in sorted(names):
+for name in sorted(names, reverse=True):
     print("Hello" , name)
