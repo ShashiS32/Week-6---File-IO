7fd11ec774508efedbccd14fd9cfe552367bf5de
Shashwat Singh <shashwats17@gmail.com>
2024-07-10 11:13:23 -0400

    very confusing

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-with open("names.csv") as file:
+students = []
+
+with open ("names.csv") as file:
     for line in file:
-        name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        name, house  = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name , reverse=True):
+    print(f"{student['name']} is in {student['house']}")
